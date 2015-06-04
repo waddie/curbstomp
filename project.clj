f3d389e6839d7003d96fa1755cc62ad99bdad195
Tom Waddington <tom@implementor.org>
2015-06-04 01:01:37 +0100

    updated

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject curbstomp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject curbstomp "0.1.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [http-kit "2.1.16"]
-                 [cheshire "5.4.0"]]
+                 [http-kit "2.1.19"]
+                 [cheshire "5.5.0"]]
   :dev-dependencies [[org.clojure/tools.nrepl "0.2.10"]]
   :plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]]
   :main ^:skip-aot curbstomp.core
